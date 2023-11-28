05613a140253a8acad4a0547df6be005d2ab6ddb
Shmair <sylvie.shamir@gmail.com>
2023-11-28 13:23:47 +0200

    Add emojis

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -13,6 +13,8 @@ export interface Game {
     background_image:string;
     parent_platforms:{platform:Platform}[];
     metacritic:number;
+    rating_top:number;
+    //rating:number; // floating number
   }
 
 const useGames = (gameQuery:GameQuery) => useData<Game>('/games', {params:{genres:gameQuery.genre?.id,platforms:gameQuery.platform?.id,ordering:gameQuery.sortOrder,search:gameQuery.searchText}},[gameQuery]); 
